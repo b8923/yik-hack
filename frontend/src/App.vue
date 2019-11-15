04c81be87b75013f1c32c62be06d9acbdf155396
David Berri <dwbwill@gmail.com>
2019-11-15 09:05:30 -0300

    Styles the header component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
+    <main class="MainApp">
+      <app-header></app-header>
+      <router-view />
+    </main>
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "App",
+
+  components: {
+    "app-header": Header
+  },
+
+  computed: {
+    loading() {
+      return false;
+    }
+  },
+
+  mounted() {}
+};
+</script>
+
 <style>
+:root {
+  --Primary-color: #2bd4b5;
+  --Primary-colorDark: #1ca38a;
+  --Secondary-color: #fff;
+  --Text-color: #2a2a2a;
+  --Muted-color: #bfbfbf;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.MainApp {
+  margin: 50px auto;
+  max-width: 368px;
 }
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="AppHeader">
+    <slot name="karma-points">
+      540
+    </slot>
+    <nav class="AppNav">
+      <a href="#" class="AppNav-link active">Hot</a>
+      <a href="#" class="AppNav-link">New</a>
+    </nav>
+    <new-post-button></new-post-button>
+  </header>
+</template>
+
+<script>
+import NewPostButton from "@/components/NewPostButton";
+
+export default {
+  name: "Header",
+
+  components: {
+    NewPostButton
+  }
+};
+</script>
+
+<style>
+.AppHeader {
+  align-items: center;
+  background-color: var(--Primary-color);
+  color: var(--Secondary-color);
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 15px 10px 15px;
+}
+
+.AppNav {
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid var(--Secondary-color);
+  border-radius: 5px;
+  color: var(--Secondary-color)
+}
+
+.AppNav .AppNav-link:visited {
+  color: var(--Secondary-color)
+}
+
+.AppNav .AppNav-link:active,
+.AppNav .AppNav-link.active {
+  color: var(--Primary-color);
+  background-color: var(--Secondary-color);
+}
+
+.AppNav .AppNav-link {
+  padding: 2px 20px;
+  text-decoration: none;
+}
+
+.AppNav-link:not(:last-child) {
+  border-right: 2px solid var(--Secondary-color);
+}
+</style>
