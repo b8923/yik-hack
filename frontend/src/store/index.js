83e63814cf5b2e65d5230dd83c0e1bf00927e181
David Berri <dwbwill@gmail.com>
2019-11-15 18:21:57 -0300

    Adds hardocded posts to the VueX store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,8 +4,75 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    currentList: "hot",
+    posts: [
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [],
+        upvotes: [1, 2, 3],
+        downvotes: [1]
+      },
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [1],
+        upvotes: [1, 2],
+        downvotes: [1, 2, 3]
+      },
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [1, 2],
+        upvotes: [1, 2, 3, 4, 5, 6, 7],
+        downvotes: [1, 2, 3]
+      },
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [],
+        upvotes: [1, 2, 3],
+        downvotes: [1]
+      },
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [1, 2],
+        upvotes: [1, 2, 3, 4, 5, 6, 7],
+        downvotes: [1, 2, 3]
+      },
+
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [],
+        upvotes: [1, 2, 3],
+        downvotes: [1]
+      },
+      {
+        text: "Lorem ipsum dolor sit amet",
+        author: {
+          _id: 125123213
+        },
+        comments: [1],
+        upvotes: [1, 2],
+        downvotes: [1, 2, 3]
+      }
+    ]
+  },
   mutations: {},
-  actions: {},
-  modules: {}
+  actions: {}
 });
