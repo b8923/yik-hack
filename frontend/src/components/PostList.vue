e315e211996400a1b3fc9ec6006a6ae902c26780
David Berri <dwbwill@gmail.com>
2019-11-15 18:21:34 -0300

    Adds some styles to the post list

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -1,11 +1,30 @@
 <template>
-  <section>
-    
+  <section class="PostList">
+    <post v-for="post in posts" :key="post._id" v-bind="post" />
   </section>
 </template>
 
 <script>
+import Post from "@/components/Post";
 export default {
   name: "PostList",
-}
+
+  components: {
+    Post
+  },
+
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    }
+  }
+};
 </script>
+
+<style scoped>
+.PostList {
+  background-color: #f2f2f2;
+  height: 100%;
+  padding-top: 10px;
+}
+</style>
