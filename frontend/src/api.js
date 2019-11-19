0d1115bae4488c45cc26097bf6aaab99a1436b6e
David Berri <dwbwill@gmail.com>
2019-11-19 10:07:57 -0300

    Adds interceptor to axios so it always sends the location with the headers

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,23 @@
 import axios from "axios";
+import store from "@/store";
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_URL,
 
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    Accept: "application/json"
   }
 });
+
+instance.interceptors.request.use(
+  config => {
+    config.headers["longitude"] = store.state.position.longitude;
+    config.headers["latitude"] = store.state.position.latitude;
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
 instance.interceptors.response.use(
   response => response,
